d52531fd4a72092b0a40e1cb87eda2d7f2ffe5c4
AnishaKB1 <anishakb4@gmail.com>
2023-12-16 08:30:23 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+
+const cors = require('cors');
+const mongoose = require('mongoose');
+const path = require('path');
+const app = new express();
+
+
+require('dotenv').config();
+app.use(express.static(path.join(__dirname,'/build')));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+require("./db/connect");
+
+const adminrouter = require('./routes/basic');
+app.use('/admin', adminrouter);
+
+
+const projectrouter = require('./routes/projecttopic');
+app.use('/project', projectrouter);
+
+const mentorrouter = require('./routes/mentordata');
+app.use('/mentor', mentorrouter);
+
+const mentordashboard = require('./routes/mentordash');
+app.use('/mentordash', mentordashboard);
+
+const submission = require('./routes/submission');
+app.use('/sub', submission);
+
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname,'/build/index.html')); }); 
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`);
+})
